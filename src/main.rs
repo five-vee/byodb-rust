e963feb5eeb5c5d042929357c7a5a10984dd076c
five-vee <8535086+five-vee@users.noreply.github.com>
2025-03-07 16:32:47 +0000

    Cargo format

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,31 @@
-use std::{io::Write as _, os::unix::fs::PermissionsExt as _};
-
 fn save_data1(path: &str, data: &[u8]) -> Result<(), std::io::Error> {
-  let file = std::fs::OpenOptions::new()
-    .write(true)
-    .create(true)
-    .truncate(true)
-    .open(path)?;
+    use std::{io::Write as _, os::unix::fs::PermissionsExt as _};
+    let file = std::fs::OpenOptions::new()
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(path)?;
 
-  let metadata = std::fs::metadata(path)?;
-  let mut permissions = metadata.permissions();
-  permissions.set_mode(0o664);
-  std::fs::set_permissions(path, permissions)?;
+    let metadata = std::fs::metadata(path)?;
+    let mut permissions = metadata.permissions();
+    permissions.set_mode(0o664);
+    std::fs::set_permissions(path, permissions)?;
 
-  let mut file = file;
-  file.write_all(data)?;
-  file.sync_all()?;
-  Ok(())
+    let mut file = file;
+    file.write_all(data)?;
+    file.sync_all()?;
+    Ok(())
 }
 
-use std::{process::Command, fs};
-
-fn main() -> Result<(), std::io::Error> {
-    let output = Command::new("mktemp")
-        .output()?;
+fn main() {
+    use std::{fs, process::Command};
+    let output = match Command::new("mktemp").output() {
+        Ok(it) => it,
+        Err(err) => {
+            eprintln!("Error running mktemp: {}", err);
+            std::process::exit(1);
+        }
+    };
 
     let temp_file_path = String::from_utf8_lossy(&output.stdout).trim().to_string();
     println!("Created file: {}", temp_file_path);
@@ -33,13 +36,18 @@ fn main() -> Result<(), std::io::Error> {
     }
     println!("Saved 'hello world' to {}", temp_file_path);
 
-    // Read the contents of the file
-    let contents = fs::read_to_string(&temp_file_path)?;
+    let contents = match fs::read_to_string(&temp_file_path) {
+        Ok(contents) => contents,
+        Err(err) => {
+            eprintln!("Error reading data: {}", err);
+            std::process::exit(1);
+        }
+    };
     println!("File contents: {}", contents);
 
-    // Delete the file
-    fs::remove_file(&temp_file_path)?;
+    if let Err(e) = fs::remove_file(&temp_file_path) {
+        eprintln!("Error deleting file: {}", e);
+        std::process::exit(1);
+    }
     println!("Deleted file: {}", temp_file_path);
-
-    Ok(())
 }
